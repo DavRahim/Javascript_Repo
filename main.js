07ed8a89351e66e53463bf71aadde0396c0bac73
Md Abdur Rahim <mdabdurrahim184275@gmail.com>
2025-05-20 23:38:14 +0600

    Logical AND (&&) Operator Logical OR (||) Operator ,Template Literals

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
 // console.log("hello world");
 
-// Logical AND (&&) Operator Logical OR (||) Operator
+// TODO: Logical AND (&&) Operator Logical OR (||) Operator
+
+function getName() {
+    return "Abdur Rahim"
+}
+
 
 let a = false;
 let b = true;
 
-console.log(a && b);
+// console.log(a && b);
+// console.log(a || b);
+// console.log(a && getName());
+// console.log(a || getName());
+
+
+// example
+// console.log(a&&b && b || a);
+
+
+// TODO: Template Literals;
+
+let firstName = "Abdur Rahim";
+let surName = "Mohammad";
+
+let prefix = "Mr.";
+
+console.log(prefix + " " + firstName + " " + surName);
+
+console.log(`${prefix} ${firstName} ${surName}`);
